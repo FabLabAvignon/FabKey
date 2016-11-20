c5352ce631cd5adc1d4c3650a0c61ac03a35dbaa
Thecakeisgit <Thecakeisplaying@gmail.com>
2016-11-20 17:09:40 +0100

    Changed database expiry emplacement, now every key can expire, or not, fixed some errors with try->except.

--- FabKey.py
+++ FabKey.py
@@ -22,54 +22,70 @@ class KeyManager:
         dQuery = self.dConn.cursor()
 
         # Execute query
-        if(dQuery.execute("SELECT keyType, keyComment FROM keyList WHERE keyValue = %s", [keyValue]) != 0):
+        if(dQuery.execute("SELECT keyType, keyOwner, keyExpiry FROM keyList WHERE keyValue = %s", [keyValue]) != 0):
             sAnsw = dQuery.fetchone()
 
-            keyType = sAnsw[0].split(":", 1)[0]
-            keyParam = sAnsw[0].split(":", 1)[1]
+            # Check if key is expired
+            keyExpiry = sAnsw[2]
 
-            # Get key type, !: Admin, 1: One time use, @: Hour check, %: Expiry
-            if (keyType == '!'):
-                return True
-
-            if (keyType == '1'):
-                dQuery.execute("DELETE FROM keyList WHERE keyValue = %s", [keyValue])
-                self.dConn.commit()
-                return True
-
-            if (keyType == '@'):
-                nowTime = datetime.now().time()
-
-                try:
-                    startTime = datetime.strptime(
-                        keyParam.split("|", 1)[0], "%H-%M").time()
-                    endTime = datetime.strptime(
-                        keyParam.split("|", 1)[1], "%H-%M").time()
-                except Exception as e:
-                    print("Warning: Key '" + keyValue + "' has configuration problem : " + str(e))
-                    return False
-
-                if (startTime < nowTime and endTime > nowTime):
-                    return True
-                else:
-                    return False
-
-            if (keyType == '%'):
+            if (keyExpiry != "*"): # If key is expirable
                 nowDate = datetime.now().date()
-
                 try:
                     expiryDate = datetime.strptime(
-                        keyParam, "%d-%m-%y").date()
-                except Exception as e:
-                    print("Warning: Key '" + keyValue + "' has configuration problem : " + str(e))
+                        keyExpiry, "%d-%m-%y").date()
+                except Exception:
+                    print("Error: Key expiry date for '"
+                        + keyValue + "' is invalid, please use * for unexpirable or date like this: DD-MM-YY.")
                     return False
 
-                if (expiryDate > nowDate):
-                    return True
-                else:
+                if (expiryDate > nowDate): # If not expired
+                    return self._validateKey(keyValue, sAnsw)
+                else: # If expired
                     dQuery.execute("DELETE FROM keyList WHERE keyValue = %s", [keyValue])
                     self.dConn.commit()
+                    print("Warning: Expired key '" + keyValue + "', removed from database.")
                     return False
+            else: # If key is unexpirable
+                return self._validateKey(keyValue, sAnsw)
+
+        else:
+            print("Warning: No key found for '" + keyValue + "'.")
+            return False
+
+    def _validateKey(self, keyValue, sAnsw):
+        keyType = sAnsw[0].split(":", 1)[0]
+
+        # Get key type, !: Admin, 1: One time use, @: Hour check
+        if (keyType == '!'):
+            return True
+
+        if (keyType == '1'):
+            dQuery.execute("DELETE FROM keyList WHERE keyValue = %s", [keyValue])
+            self.dConn.commit()
+            return True
+
+        if (keyType == '@'):
+            try:
+                keyParam = sAnsw[0].split(":", 1)[1]
+            except Exception:
+                print("Warning: Key '" + keyValue + "' has no parameter, expected parameter.")
+                return False
+
+            nowTime = datetime.now().time()
+
+            try:
+                startTime = datetime.strptime(
+                    keyParam.split("|", 1)[0], "%H-%M").time()
+                endTime = datetime.strptime(
+                    keyParam.split("|", 1)[1], "%H-%M").time()
+            except Exception as e:
+                print("Warning: Key '" + keyValue + "' has configuration problem : " + str(e))
+                return False
+
+            if (startTime < nowTime and endTime > nowTime):
+                return True
+            else:
+                return False
 
 class SMSHandler:
     def __init__(self):
@@ -86,4 +102,4 @@ if __name__ == '__main__':
         Config.get("db", "passwd"),
         Config.get("db", "name"))
 
-    print(keyMgr.checkKey("AG4575"))
+    print(keyMgr.checkKey("TC0989"))
